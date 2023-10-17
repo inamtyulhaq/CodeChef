492fe95041133e73689256e10947fd2ebf4aadd8
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-17 17:39:03 +0500

    Update Chef and Instant Noodles.c

--- Chef and Instant Noodles.c
+++ Chef and Instant Noodles.c
@@ -1,8 +1,15 @@
+/*Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 
+1 minute to cook. Chef's restaurant has X stoves and only 1 packet can be cooked in a single stove at any minute.
+How many customers can Chef serve in Y minutes if each customer orders exactly 1 packet of noodles?*/
+// Difficulty Rating: 456
 
+//Explanation:
+/*I simply asked the user to input two numbers and 
+and then did their multiplication. Numbers inputted are the
+number of stoves and minutes and the output is the number of
+maximum customers the chef can serve.*/
 
-
-
-
+// Solution:
 
 #include <stdio.h>
 
