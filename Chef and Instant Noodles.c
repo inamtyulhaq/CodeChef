5b0a51ab2c66c5552da40d3e807298aeb2f15e3c
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-17 17:32:08 +0500

    Create Chef and Instant Noodles.c

---
+++ Chef and Instant Noodles.c
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	int X,Y,Cust;
+	printf("");
+	scanf( "%d",&X);
+	printf("");
+	scanf( "%d",&Y);
+	Cust= X * Y;
+	printf("%d",Cust);
+	return 0;
+}
