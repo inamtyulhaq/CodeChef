50e5a40b66bf55474234b54d26eff9cf18a23f41
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-18 08:43:50 +0500

    Update Add Two Numbers.c

--- Add Two Numbers.c
+++ Add Two Numbers.c
@@ -4,7 +4,10 @@ write a program to add these two numbers and output the sum.*/
 
 //Explanation:
 /*In the first line, I took the number of test cases from the user and stored them
-in the variable "T". After that, I used*/
+in the variable "T". After that, I used the for loop and gave "i" the input value of 0 and
+ran it till it's less than the value of T. Then, in the loop, I simply took the values of A
+and B from the user and added them in another variable "sum". Then, I printed the integer 
+value of the sum.*/
 
 // Solution:
 
