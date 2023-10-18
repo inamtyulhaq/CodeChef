625d884363a9c5bfa1ab0c2f34181587eb061a4d
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-18 09:07:01 +0500

    Update Good Turn.c

--- Good Turn.c
+++ Good Turn.c
@@ -5,7 +5,12 @@ find whether the turn was good.*/
 // Difficulty Rating: 238
 
 //Explanation:
-/*This problem was almost same as the previous one. */
+/*This problem was almost same as the previous one, "Add two numbers". 
+In the first line, I took the number of test cases from the user and stored them
+in the variable "T". After that, I used the for loop and gave "i" the input value of 0 and
+ran it till it's less than the value of T. Then, in the loop, I simply took the values of X
+and Y from the user and added them in another variable "sum". Then, I used if..else loop and if
+the sum is greater than 6, I printed "Yes", else print "No".*/
 
 // Solution:
 
