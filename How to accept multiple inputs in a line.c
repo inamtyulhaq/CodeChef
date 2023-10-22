aeab34caf1406c6766a830380379d24dab9140df
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-22 14:00:49 +0500

    Create How to accept multiple inputs in a line.c

---
+++ How to accept multiple inputs in a line.c
@@ -0,0 +1,10 @@
+//simply how to take multiple prints in one line and print them in one line.
+
+#include <stdio.h>
+
+int main() {
+    int A,B,C;
+    scanf("%d ""%d ""%d",&A,&B,&C);
+    printf("%d ""%d ""%d",A,B,C);
+    return 0;
+}
