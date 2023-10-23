a70a76e9f02d986c68b8368f5352dbeeba7dc18f
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-23 21:47:47 +0500

    Create How to accept multiple integers on separate line.c

---
+++ How to accept multiple integers on separate line.c
@@ -0,0 +1,11 @@
+//Update the blanks below to solve the problem
+#include <stdio.h>
+
+int main() {
+    int A,B,C,D,E,F,G,H,I;
+    scanf("%d","%d",&A,&B);
+    scanf("%d","%d","%d",C,D,E);
+    scanf("%d","%d","%d","%d",F,G,H,I);
+    printf("%d","%d","%d","%d","%d","%d","%d","%d","%d",)
+    return 0;
+}
