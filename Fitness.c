7e6b31310b6fefbe58adfa80cef0f20dd1c72b33
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-24 21:35:47 +0500

    Update Fitness.c

--- Fitness.c
+++ Fitness.c
@@ -1,4 +1,16 @@
 /*Chef wants to become fit for which he decided to walk to the office and return home by walking. 
 It is known that Chef's office is X km away from his home. If his office is open on 5 days 
 in a week, find the number of kilometers Chef travels through office trips in a week.*/
+
 // Difficulty Rating: 258
+
+/**/
+
+//Solution:
+
+#include <stdio.h>
+
+int main(void) {
+	
+}
+
