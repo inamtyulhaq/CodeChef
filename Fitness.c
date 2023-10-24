201cb1e58a02f1f8dbff598048a6e018e6a0ff46
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-24 21:45:27 +0500

    Update Fitness.c

--- Fitness.c
+++ Fitness.c
@@ -4,8 +4,24 @@ in a week, find the number of kilometers Chef travels through office trips in a 
 
 // Difficulty Rating: 258
 
-/**/
+/*In this problem, I took the number of test cases. Then, I used for loop,
+so that the program runs till it reaches the number of total test cases. Simply used
+scanf function, multiplied the X with 10 and simply outputted the value.*/
 
 //Solution:
+
+#include <stdio.h>
+
+int main(void) {
+	int T,X,Y;
+	scanf("%d", &T);
+	for(int i=0;i<T;i++){
+	scanf("%d", &X);
+	Y=5*2*X;
+	printf("%d\n", Y);
+	}
+	return 0;
+}
+
 
 
