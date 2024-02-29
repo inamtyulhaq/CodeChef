fcfd84b243942b5e06b864919db8736151c00dd1
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-29 21:55:52 +0500

    Create START109-Cricket World Cup Qualifier.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START109-Cricket World Cup Qualifier.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	if(X>=12)
+	{
+	    cout << "YES" << endl;
+	}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+	return 0;
+}
