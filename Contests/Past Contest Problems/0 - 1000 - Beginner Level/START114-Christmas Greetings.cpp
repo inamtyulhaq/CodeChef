b4ef97bb1837509b35952a8ac992a8abf09993fc
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-29 21:58:58 +0500

    Create START114-Christmas Greetings.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START114-Christmas Greetings.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	if(X==25)
+	{
+	    cout << "CHRISTMAS" << endl;
+	}
+	else
+	{
+	    cout << "ORDINARY" << endl;
+	}
+    return 0;
+}
