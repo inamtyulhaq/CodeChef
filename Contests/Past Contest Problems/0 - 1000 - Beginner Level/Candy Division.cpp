59987702b7e05c371520567b4d6120cb6534020c
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-01 11:20:57 +0500

    Create Candy Division.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/Candy Division.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,N;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> N;
+	    if(N % 3 == 0)
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+    return 0;
+}
