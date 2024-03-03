a7f3d622f2dc4f189449480515a68e6770974805
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-03 09:56:19 +0500

    Create START73-Chef Plays Ludo.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START73-Chef Plays Ludo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T, X;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X;
+	    if(X==6)
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+    return 0;
+}
