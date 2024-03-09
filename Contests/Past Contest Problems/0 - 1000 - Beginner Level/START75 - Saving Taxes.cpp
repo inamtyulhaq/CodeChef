b66e7fdcbc8282a6255a7243e026bb8891e95cbd
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-09 22:54:59 +0500

    Create START75 - Saving Taxes.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START75 - Saving Taxes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T, X, Y;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X >> Y;
+	    int Z = X - Y;
+	    cout << Z << endl;
+	}
+    return 0;
+}
