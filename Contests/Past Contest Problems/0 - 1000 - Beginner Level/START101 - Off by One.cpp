6331cc772e2163de2baa9bcaab5c2d10c9499742
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-11 21:07:07 +0500

    Create START101 - Off by One.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START101 - Off by One.cpp
@@ -0,0 +1,19 @@
+// Question:
+// https://www.codechef.com/problems/OFFBY1
+
+// Explanation:
+// It is extremely simple.
+// Simply add the number.
+// Just add 1 alongside the sum in the cout.
+// Just add in the cout and add 1 alongside it.
+
+// Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << 1 << endl;
+    return 0;
+}
