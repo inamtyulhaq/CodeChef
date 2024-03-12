05ccf52180b98d2ee15a3810566cba821841bb43
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-12 13:56:04 +0500

    Create START44 - Biryani Classes.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START44 - Biryani Classes.cpp
@@ -0,0 +1,20 @@
+// Question:
+// https://www.codechef.com/problems/BIRYANI
+
+// Explanation:
+// Simply input two integers and cout their product.
+
+// Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T, X, Y;
+	cin >> T;
+	while(T--)
+	{
+	    cin >> X >> Y;
+	    cout << X*Y << endl;
+	}
+	return 0;
+}
