f3b5bc1390c0481950aadb1db2827a93870a0333
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-21 07:17:28 +0500

    Create Overspeeding.cpp

---
+++ Contests/Starters 125 Division 4 (Rated)/Overspeeding.cpp
@@ -0,0 +1,23 @@
+// Question:
+// https://www.codechef.com/START125D/problems/CCOV
+
+// Explanation:
+// If the speed is less than 40 (40 is inclusive), output "NO" else output "YES".
+
+// Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int S;
+	cin >> S;
+	if(S<=40)
+	{
+	    cout << "NO" << endl;
+	}
+	else
+	{
+	    cout << "YES" << endl;
+	}
+    return 0;
+}
