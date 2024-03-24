9a7ee5ddedf3b043dc82c624d9f8b830b5e43c4b
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-24 13:10:02 +0500

    Create START58 - Reach the target.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START58 - Reach the target.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/problems/REACHTARGET
+
+// Simply subtract current scores from the target given.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int X, Y;
+	    cin >> X >> Y;
+	    int Z = X - Y;
+	    cout << Z << endl;
+	}
+    return 0;
+}
