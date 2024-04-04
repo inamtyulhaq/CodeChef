e1bf75aff5d45c631ae4b4e0fab0197ad25af064
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-04-05 04:46:52 +0500

    Create Reach 5 Star.cpp

---
+++ Contests/Starters 128 Division 4 (Rated)/Reach 5 Star.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/START128D/problems/R5S
+
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y;
+     cin >> X >> Y;
+     if(X+Y>=2000)
+     {
+         cout << "YES" << endl;
+     }
+     else
+     {
+         cout << "NO" << endl;
+     }
+     return 0;
+
+}
