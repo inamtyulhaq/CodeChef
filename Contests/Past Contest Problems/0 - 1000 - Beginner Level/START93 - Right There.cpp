4a5b8f11433ccf2370bf60b72208271c01729ff3
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-04-16 12:26:10 +0500

    Create START93 - Right There.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START93 - Right There.cpp
@@ -0,0 +1,25 @@
+// https://www.codechef.com/problems/RIGHTTHERE
+
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int N, X;
+	    cin >> N >> X;
+	    if(X>=N)
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+    return 0;
+}
