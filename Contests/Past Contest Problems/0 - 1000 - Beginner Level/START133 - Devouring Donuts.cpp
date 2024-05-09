3ce97bed88a486fa88f7102c0e6f1a81ce804fe2
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-09 18:40:24 +0500

    Create START133 - Devouring Donuts.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START133 - Devouring Donuts.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/DEVDON
+
+// simply multiply number of donuts with the calories.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y;
+	cin >> X >> Y;
+	X*=Y;
+	cout << X << endl;
+
+}
