662c3a3dbed1dff410a4f91d72518332c8dcae38
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-05-17 12:42:20 +0500

    Create START84 - Chef and Donation.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START84 - Chef and Donation.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/DNATION
+
+// simple subtract X from Y
+// that's the amount given for charity
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int X, Y;
+	    cin >> X >> Y;
+	    int C = Y-X;
+	    cout << C << endl;
+	}
+    return 0;
+}
