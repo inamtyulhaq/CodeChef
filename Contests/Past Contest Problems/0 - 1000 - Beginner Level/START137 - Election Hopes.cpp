8a1c9f0debc80be367cf4c2301bb8f8416d24639
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-06 08:27:43 +0500

    Create START137 - Election Hopes.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START137 - Election Hopes.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/problems/ELHP
+
+// use a simple if else condition
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y;
+	cin >> X >> Y;
+	if(X>=2*Y)
+	{
+	   cout << "YES" << endl;
+	}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+	
+    return 0;
+}
