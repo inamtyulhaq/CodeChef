86dbdf13b78de107149f0a1825ebcdf8ae7cfe31
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-11 13:00:31 +0500

    Create START121 - Leg Space.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START121 - Leg Space.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/LEGSP
+
+// use if else to satisfy the simple relation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	if(M-N>=1)
+	{
+	    cout << "YES" << endl;
+	}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+	return 0;
+}
