9f2fca731f3367a28699114c6005906334671205
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-12 13:12:23 +0500

    Create START100 - Air Quality Index.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START100 - Air Quality Index.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/AIRINDEX
+
+// use if-else based on the condition
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	if(X<100)
+	{
+	    cout << "YES" << endl;
+	}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+    return 0;
+}
