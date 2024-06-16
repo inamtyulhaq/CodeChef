4ed14487cde9f512c65176fcaf5501b2d63e4557
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-16 10:02:41 +0500

    Create START83 - Couple Game.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START83 - Couple Game.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/COUGAME
+
+// cout the difference
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int G, B;
+	    cin >> G >> B;
+	    cout << B-G << endl;
+	}
+    return 0;
+}
