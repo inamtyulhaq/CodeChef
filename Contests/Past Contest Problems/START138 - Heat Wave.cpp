bc2a85ddc2ac16b8bb57dc2db152d273371c4f31
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-17 10:52:59 +0500

    Create START138 - Heat Wave.cpp

---
+++ Contests/Past Contest Problems/START138 - Heat Wave.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/HEATWAVE
+
+// use if-else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y;
+	cin >> X >> Y;
+	if(Y > X)
+	{
+	    cout << "YES" << endl;
+	}
+	else
+	{
+	    cout << "NO" << endl;
+	}
+    return 0;
+}
