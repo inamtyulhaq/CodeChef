957b4b25cbd9b40c8acdf5bfd5de2aaae9a4e40c
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-06-28 12:51:20 +0500

    Create START119 - Advitiya.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START119 - Advitiya.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/ADVITIYA1
+
+// use if else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if(N == 16 || N == 17 || N == 18)
+	{
+	    cout << "ADVITIYA" << endl;
+	}
+	else
+	{
+	    cout << "WAITING FOR ADVITIYA" << endl;
+	}
+    return 0;
+}
