ef07f18ced86f7c12879d85ab705f3f2099b76ef
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-07-22 22:02:26 +0500

    Create START142 - Writing Speed.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START142 - Writing Speed.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/WRITINGSPEED
+
+// use if else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(5*x<=60)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
