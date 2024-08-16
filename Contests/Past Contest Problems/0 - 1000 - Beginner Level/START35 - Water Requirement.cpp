2ebaac09398fd6eba0f338d0859ef3ab598d87b9
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2024-08-16 19:19:43 +0500

    Create START35 - Water Requirement.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START35 - Water Requirement.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/WATERREQ
+
+// for every hour, 2 liters of water are required
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        cout << n*2 << endl;
+    }
+    return 0;
+}
