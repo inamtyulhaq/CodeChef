56244b101adf2a680618c3aa314720573b567427
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2024-08-21 11:37:28 +0500

    Create START95 - Donation Drive.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START95 - Donation Drive.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/DONDRIVE
+
+// cout n-x
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        cout << n-x << endl;
+    }
+    return 0;
+}
