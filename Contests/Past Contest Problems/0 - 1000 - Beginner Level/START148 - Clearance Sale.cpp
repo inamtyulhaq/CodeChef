4683a41e8befa763aac83e252a88dd8836bab970
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2024-08-22 23:39:50 +0500

    Create START148 - Clearance Sale.cpp

---
+++ Contests/Past Contest Problems/0 - 1000 - Beginner Level/START148 - Clearance Sale.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/CLEARANCE
+
+// return the shirts
+// plus half the number of shirts as the free ones
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << x+(x/2) << endl;
+    return 0;
+}
