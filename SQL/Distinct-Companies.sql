a50ea3df41bbf11225a3c191dc8c2ae3fb8619a6
inamtyulhaq <inamtyulhaq@gmail.com>
2024-10-31 09:02:23 +0500

    SQL Problem

---
+++ SQL/Distinct-Companies.sql
@@ -0,0 +1,7 @@
+-- https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP13
+
+-- use DISTINCT keyword
+
+-- your code goes here
+SELECT DISTINCT company_name
+FROM Works
