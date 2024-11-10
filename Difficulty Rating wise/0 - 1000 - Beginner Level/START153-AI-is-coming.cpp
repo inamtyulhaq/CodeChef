efbfc3124c31b50968dad634b66564eec2f5da28
inamtyulhaq <inamtyulhaq@gmail.com>
2024-11-11 04:41:10 +0500

    An easy problem

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/START153-AI-is-coming.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/AICOM
+
+// use if-else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if(x<=60)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
