99c32a4f71685cbeae57bbf43df32fe24bf4e029
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2024-12-26 23:57:17 +0500

    Cricket World Cup Qualifier

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/CWC23QUALIF-Cricket-World-Cup-Qualifier.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+
+// use if else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x >= 12)
+    {
+        cout << "yes\n";
+    }
+    else
+    {
+        cout << "no\n";
+    }
+    return 0;
+}
