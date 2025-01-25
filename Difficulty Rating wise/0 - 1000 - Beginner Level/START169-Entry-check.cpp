f3858d7c63c456fabf4ec886a657da265587c199
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2025-01-25 23:19:12 +0500

    entry check

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/START169-Entry-check.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/problems/P1169
+
+// use if-else
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    if(x >= 10)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
