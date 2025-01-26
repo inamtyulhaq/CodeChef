039e72d2a8010ff512893e46f0a9bbd4bcc13c36
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2025-01-26 20:41:56 +0500

    Moneymaking

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/START161-Moneymaking.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/NUGGET
+
+// simply cout after multiplying
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y;
+    cin >> x >> y;
+    cout << x*5000 + y*9800 << endl;
+    return 0;
+}
