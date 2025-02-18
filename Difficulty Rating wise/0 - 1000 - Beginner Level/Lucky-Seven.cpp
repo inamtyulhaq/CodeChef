596bca1131dde731e9c6556c1bda15005c3e943c
inamtyulhaq <inamtyulhaq@gmail.com>
2025-02-18 23:53:41 +0500

    Lucky Seven - C++

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/Lucky-Seven.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+
+// simply print the letter at the 6th index
+// at the 7th position
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;
+    cin >> s;
+    cout << s[6] << endl;
+    return 0;
+}
