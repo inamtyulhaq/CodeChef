3ed84405a0e8d7fcf3ef264060ef1b19cd31bf56
inamtyulhaq <inamtyulhaq@gmail.com>
2025-02-19 19:19:09 +0500

    Clear Day - C++

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/Clear-Day.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+
+// simply subtract from 7
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y;
+    cin >> x >> y;
+    cout << 7 - x - y << endl;
+    return 0;
+}
