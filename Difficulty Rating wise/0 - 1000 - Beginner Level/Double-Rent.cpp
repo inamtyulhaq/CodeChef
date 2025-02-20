0daccd97ef28946e4c0a2e69697e79d83ecd117a
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2025-02-20 23:38:16 +0500

    Double Rent - C++

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/Double-Rent.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+
+// simply double and output
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin >> x;
+    cout << x*2 << endl;
+    return 0;
+}
