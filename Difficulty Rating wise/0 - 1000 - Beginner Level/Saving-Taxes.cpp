a47cf693029722fae031db3a7b701aaafb4c603f
inamtyulhaq <111003676+inamtyulhaq@users.noreply.github.com>
2025-02-21 11:04:23 +0500

    Saving Taxes - C++

---
+++ Difficulty Rating wise/0 - 1000 - Beginner Level/Saving-Taxes.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+
+// cout x-y 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x-y << endl;
+    }
+    return 0;
+}
